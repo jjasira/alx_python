dd2631026b3df0d3f3ab3b4e779bde9a49536a05
James Asira <jjasira2016@gmail.com>
2023-11-26 09:13:30 +0300

    revision

--- python-functions/5-prime.py
+++ python-functions/5-prime.py
@@ -1,12 +1,10 @@
 def is_prime(number):
-    if number <= 1:
+    if number < 2:
         return False
-    elif number > 1:
-        for i in range(2, number):
-            if (number % i) == 0:
-                return False
-            else:
-                return True
-
+    for i in range(2, int(number**0.5) + 1):
+        if number % i == 0:
+            return False
+    return True
+    
 
 
