13ef69b8c4cf759791d034fa5220bde612e3f62f
James Asira <jjasira2016@gmail.com>
2023-11-26 08:55:26 +0300

    this is a commit message for task 6

---
+++ python-functions/5-prime.py
@@ -0,0 +1,12 @@
+def is_prime(number):
+    if number == 1:
+        return False
+    elif number > 1:
+        for i in range(2, number):
+            if (number % i) == 0:
+                return False
+            else:
+                return True
+
+
+
