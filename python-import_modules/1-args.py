6dbef980481a1b2f5ff526676690b844a09aac16
James Asira <jjasira2016@gmail.com>
2023-12-02 16:07:19 +0300

    revision

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,19 +1,15 @@
+#!/usr/bin/python3
+import sys
+
 if __name__ == "__main__":
-    from variable_load_2 import a
-argv = ["Heloo"]
-
-length = len(argv)
-
-if length == 0:
-    print("{} arguments.".format(length))
-elif length == 1:
-    print("{} argument: ".format(length))
-    for i, element in enumerate(argv):
-        print("{}: {}".format((i+1), element))
-elif length > 1:
+    args_count = len(sys.argv) - 1  # The first element in sys.argv is the script name
     
-    print("{} arguments: ".format(length))
-    for i, element in enumerate(argv):
-        print("{}: {}".format((i+1), element))
+    if args_count == 0:
+        print("0 arguments.")
+    elif args_count == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(args_count))
 
-        
+    for i in range(1, args_count + 1):
+        print("{}: {}".format(i, sys.argv[i]))
