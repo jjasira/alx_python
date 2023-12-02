a5e7123c8b9a3c6280a2426222cdf4e2c0a11083
James Asira <jjasira2016@gmail.com>
2023-12-02 15:15:34 +0300

    task 4

---
+++ python-more_data_structures/3-best_score.py
@@ -0,0 +1,18 @@
+def best_score(a_dictionary):
+    
+    if len(a_dictionary) == 0:
+        return None
+    
+    max_value = 0
+
+    for key in a_dictionary:
+        if a_dictionary[key] > max_value:
+            max_value = a_dictionary[key]
+    
+    for key in a_dictionary:
+        if a_dictionary[key] == max_value:
+            return key
+    
+    
+   
+
