a72114b773f6cf2b5b4cf265835b35a9a449ccde
James Asira <jjasira2016@gmail.com>
2023-12-02 12:44:19 +0300

    revision

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -2,10 +2,12 @@ def safe_print_division(a, b):
     try:
         result = a/ b
         
-    except ZeroDivisionError:
+    except:
         result = None
     finally:
         print("inside result: {}".format(result))
         print("{} / {} = {}".format(a, b, result))
+
+    
 
     
