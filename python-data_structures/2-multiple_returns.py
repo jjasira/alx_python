a666d9cee7f60c8369d3a010a00ce943ec367fbc
James Asira <jjasira2016@gmail.com>
2023-12-02 13:42:36 +0300

    revision

--- python-data_structures/2-multiple_returns.py
+++ python-data_structures/2-multiple_returns.py
@@ -4,7 +4,7 @@ def multiple_returns(sentence):
         first = None
     else:
         first = sentence[0]
-    print("Length: {:d} - First character: {}".format(length, first))
+    return (length, first)
 
 
 
