cde55bf2eebdaac4278752b0baaefd2cd85f35ed
James Asira <jjasira2016@gmail.com>
2023-12-02 12:47:38 +0300

    revision

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -1,12 +1,13 @@
 def safe_print_division(a, b):
-    try:
-        result = a/ b
-        break
-    except:
-        result = None
-    finally:
-        print("inside result: {}".format(result))
-        print("{} / {} = {}".format(a, b, result))
+    while True:
+        try:
+            result = a/ b
+            break
+        except:
+            result = None
+        finally:
+            print("inside result: {}".format(result))
+            print("{} / {} = {}".format(a, b, result))
 
     
 
