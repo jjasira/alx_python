daf651b25dfe94d5b0ee50e5e588e9ac76fd69b0
James Asira <jjasira2016@gmail.com>
2023-12-02 15:29:34 +0300

    revision

--- python-more_data_structures/3-best_score.py
+++ python-more_data_structures/3-best_score.py
@@ -1,19 +1,20 @@
 def best_score(a_dictionary):
     if a_dictionary == None:
         return None
-    if len(a_dictionary) == 0:
+    elif len(a_dictionary) == 0:
         return None
+    else:
     
-    max_value = 0
+        max_value = 0
 
-    for key in a_dictionary:
-        if a_dictionary[key] > max_value:
-            max_value = a_dictionary[key]
-    
-    for key in a_dictionary:
-        if a_dictionary[key] == max_value:
-            return key
-    
+        for key in a_dictionary:
+            if a_dictionary[key] > max_value:
+                max_value = a_dictionary[key]
+        
+        for key in a_dictionary:
+            if a_dictionary[key] == max_value:
+                return key
+        
     
 my_dict = None
 
