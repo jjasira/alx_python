f4ddf3b965eaeca209142a332289e5211b5a5e9e
James Asira <jjasira2016@gmail.com>
2023-12-02 16:01:33 +0300

    revision

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -12,8 +12,3 @@ def safe_print_division(a, b):
         return result
     finally:
         print("{} / {} = {}".format(a, b, result))
-
-# Example usage:
-a = 10
-b = 2
-result = safe_print_division(a, b)
