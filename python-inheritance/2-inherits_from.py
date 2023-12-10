58d9efcc28226e572707d42b9de4b07c94d8f800
James Asira <jjasira2016@gmail.com>
2023-12-10 16:45:00 +0300

    revision

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -1,4 +1,4 @@
 """we define a function that checks if an object inherits from a class"""
 def inherits_from(obj, a_class):
     """we will use the type and isinstance methods"""
-    return isinstance(obj, a_class) or type(obj) == a_class
+    return issubclass(type(obj), a_class) and type(obj) != a_class
