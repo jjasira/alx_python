aff8c8d67c6126bc9b890b9af0f0062746eed530
James Asira <jjasira2016@gmail.com>
2023-12-10 14:54:08 +0300

    second task commit

---
+++ python-classes/1-square.py
@@ -0,0 +1,21 @@
+
+"""
+    In this module we will learn about class declarations
+"""
+class Square:
+    """
+    This is the class declaration
+    """
+    def __init__(self, size=0):
+        """
+        this is a private instance variable
+        """
+        self.__size = size
+        if self.__size < 0:
+            raise ValueError("size must be >= 0")
+        """raise a value error exception"""
+        if type(self.__size) is not int:
+            raise TypeError("size must be an integer")
+        
+        """raise a type error exception"""
+            
