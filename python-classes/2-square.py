888ff9e5e3c94a42700910856f988e3ac75edb92
James Asira <jjasira2016@gmail.com>
2023-12-10 14:59:49 +0300

    commit message for task 3

---
+++ python-classes/2-square.py
@@ -0,0 +1,25 @@
+
+"""
+    In this module we will learn about class declarations
+"""
+class Square:
+    """
+    This is the class declaration
+    """
+    def __init__(self, size=0):
+        """
+        this is a private instance variable
+        """
+        self.__size = size
+        if type(self.__size) is not int:
+            raise TypeError("size must be an integer")
+        
+        """raise a type error exception"""
+        if self.__size < 0:
+            raise ValueError("size must be >= 0")
+        """raise a value error exception"""
+        
+    def area(self):
+        return self.__size ** 2
+        """This method calculates the area of the square"""
+    
