ee31adbf81fb46991b6783e5ae879cdcf785c7fe
James Asira <jjasira2016@gmail.com>
2023-12-10 16:59:18 +0300

    commit for task 5

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -1,6 +1,4 @@
 """we define our base class"""
 class BaseGeometry():
     """This is our base class"""
-    def area(self):
-        """This method raises an exception"""
-        raise Exception("area() is not implemented")
+    pass
---
+++ python-inheritance/4-base_geometry.py
@@ -0,0 +1,6 @@
+"""we define our base class"""
+class BaseGeometry():
+    """This is our base class"""
+    def area(self):
+        """This method raises an exception"""
+        raise Exception("area() is not implemented")
