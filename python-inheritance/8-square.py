d35c1811829ad6a2cd9789cb316e912be6bc3f45
James Asira <jjasira2016@gmail.com>
2023-12-10 17:47:09 +0300

    commit for task 9

--- python-inheritance/8-square.py
+++ python-inheritance/8-square.py
@@ -28,12 +28,24 @@ class Rectangle(BaseGeometry):
         """this method calculates the area"""
         return self.__width * self.__height
     
+    def __str__(self) -> str:
+        """return the string representation of the rectangle."""
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+    
+    def print(self):
+        """print the rectangle"""
+        print(str(self))
+    
 
 
 
 class Square(Rectangle):
+    """This class inherits from the Rectangle class."""
     def __init__(self, size):
+        """We define a field for the size and validate it"""
+        self.integer_validator("size", size)
         self.__size = size
 
     def area(self):
+        """we define an area method that overides the superclass"""
         return self.__size ** 2
