ee03aebbce3f0af196b93378e9b789d84114ccca
James Asira <jjasira2016@gmail.com>
2023-12-19 11:44:56 +0300

    my first commit

---
+++ models/README.md
@@ -0,0 +1,1 @@
+We will dive deeper into python programming using OOP and testing
---
+++ models/base.py
@@ -0,0 +1,12 @@
+"""In this module we will explore python classes further"""
+class Base:
+    """This is a private class attribut"""
+    __nb_objects = 0
+    def __init__(self, id=None):
+        """We will test if the id property is not None then we will assign it"""
+        if id != None:
+
+            self.id = id
+        else:
+            __nb_objects += 1
+            self.id = __nb_objects
