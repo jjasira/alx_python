ac774158f4107b4c7790cd5d6760a79c0ab617de
James Asira <jjasira2016@gmail.com>
2023-12-19 17:15:20 +0300

    github api

---
+++ python-network_1/6-my_github.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+import requests
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: ./github_id.py <username> <personal_access_token>")
+        sys.exit(1)
+
+    username = sys.argv[1]
+    token = sys.argv[2]
+
+    """ Set the GitHub API endpoint for the authenticated user"""
+    url = "https://api.github.com/user"
+
+    """ Set the authentication credentials"""
+    auth = (username, token)
+
+    """Send a GET request to the GitHub API"""
+    response = requests.get(url, auth=auth)
+
+    if response.status_code == 200:
+        user_data = response.json()
+        user_id = user_data['id']
+        print(f"GitHub User ID: {user_id}")
+    else:
+        print(f"Error: {response.status_code} - {response.text}")
