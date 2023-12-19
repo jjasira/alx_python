44048f3fb7fbcc508ae2225af3e43c406893f6df
James Asira <jjasira2016@gmail.com>
2023-12-19 16:56:02 +0300

    commit for the second task

---
+++ python-network_1/1-hbtn_header.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""we will use the folloeing two packages"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    """we will take a url"""
+    url = sys.argv[1]
+    
+    """Send a get request for the data that we want"""
+    response = requests.get(url)
+    
+    x_request_id = response.headers.get('X-Request-Id')
+    
+    print(f"The value of X-Request-Id is: {x_request_id}")
