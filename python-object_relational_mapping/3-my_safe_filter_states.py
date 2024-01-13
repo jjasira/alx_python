82241fe14450a01834dafb4ce4063059ccd876af
James Asira <jjasira2016@gmail.com>
2024-01-13 13:19:52 +0300

    revision

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -15,7 +15,7 @@ if __name__ == '__main__':
     cursor = db.cursor()
 
     """We execute the required query"""
-    query = "SELECT * FROM states WHERE BINARY name = %s ORDER BY id ASC"
+    query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
     cursor.execute(query, (state_name))
 
     """Fetch all the rows"""
