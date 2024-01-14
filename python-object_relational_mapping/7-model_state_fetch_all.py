525d27cf8a4eb059ad839bcdd7cfacdfb18654cb
James Asira <jjasira2016@gmail.com>
2024-01-14 10:00:13 +0300

    commit for task 3

---
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,32 @@
+"""We will import all the required modules"""
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+import sys
+
+"""To prevent the script from executing when importing"""
+if __name__ == '__main__':
+    """We assign the command line arguments"""
+    username, password, database = sys.argv[1:]
+
+    """create an engine to connect to the database"""
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(username, password, database), pool_pre_ping=True)
+
+    """create a session to interact with the database"""
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    try:
+        """Write a query with sqlalchemy to fetch all the Objects and Order by id"""
+        states = session.query(State).order_by(State.id).all()
+
+        """Display the results"""
+        for state in states:
+            print("{}: {}".format(state.id, state.name))
+
+    except Exception as e:
+        print("Error:", e)
+    finally:
+        """Close the session"""
+        session.close()
